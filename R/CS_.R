e1be02695b213130b10ae773902be0f6d0f5c922
Lore Merdrignac <37700893+loremerdrignac@users.noreply.github.com>
2023-09-29 16:46:34 +0200

    Allowinbg pooping over exposure in CS too (related to issue#7)

--- R/CS_.R
+++ R/CS_.R
@@ -4,30 +4,29 @@
 
 # ===========================================================================
 
-cs <- CS <- function(  x,
-                 cases,
-                 exposure,
-                 exact = F,
-                 full = FALSE,
-                 title = "CS"
+cs <- CS <- function(x,
+                     cases,
+                     exposure,
+                     exact = F,
+                     full = FALSE,
+                     title = "CS"
 ) UseMethod("CS", x)
 
-CS.data.frame <- function(  x,
-                            cases,
-                            exposure,
-                            exact = F,
-                            full = FALSE,
-                            title = "CS"
+CS.data.frame <- function(x,
+                          cases,
+                          exposure,
+                          exact = F,
+                          full = FALSE,
+                          title = "CS"
 )
 {
-
+  
   # ---------------------------------------------------------------------------
   # Internal functions
   # ---------------------------------------------------------------------------
-
   
-  computeDiffRiskCI <- function(RE, RU, NE, NU)
-  {
+  
+  computeDiffRiskCI <- function(RE, RU, NE, NU) {
     A = RE - RU;
     B = (RE * (1-RE))/NE;
     C = (RU * (1-RU))/NU;
@@ -38,13 +37,38 @@ CS.data.frame <- function(  x,
     return(c(R2, R1));
   }
   
-  .Cases <- x[, cases]
-  .Exposure <- x[, exposure]
+  
+  # Allowing cases to be either variable or charater string
+  r <- try(class(cases),TRUE)
+  if ( ! inherits(r, "try-error")) {
+    if ("character" %in% r ) {
+      .cases <- cases
+    } else {
+      stop("cases must be either the column name in character format or the variable name")
+    }
+  } else {
+    .cases <- as.character(substitute(cases))
+  }
+  
+  # Allowing exposure to be either variable or charater string
+  r <- try(class(exposure),TRUE)
+  if ( ! inherits(r, "try-error")) {
+    if ("character" %in% r ) {
+      .expos <- exposure
+    } else {
+      stop("exposure must be either the column name in character format or the variable name")
+    }
+  } else {
+    .expos <- as.character(substitute(exposure))
+  }
+  
+  .Cases <- x[, .cases]
+  .Exposure <- x[, .expos]
   .T1 = title
   .T2 = title
   
   # PLabel = ifelse(exact == T, "p-value (Fisher)", "p-value (chi2)")
-
+  
   Rownames1 <- c("Exposed", "Unexposed", "Total")
   Colnames1 <- c("Cases", "Non Cases", "Total", "Risk")
   PLabel <- c("chi2(1)", "Pr>chi2")
@@ -56,28 +80,28 @@ CS.data.frame <- function(  x,
   I1E0 = FR[2,1]
   I0E0 = FR[1,1]
   I0E1 = FR[1,2]
-
+  
   CHI2 = computeKHI2(FR[1,1], FR[2,1], FR[1,2], FR[2,2]);
-
+  
   FISHER <- NA
   if (exact == TRUE) {
     FISHER = computeFisher(FR[1,1], FR[2,1], FR[1,2], FR[2,2]);
     PLabel = c(PLabel, "Fisher p.value")
   }
-
+  
   # Compute Total
   # ---------------------------------------------------------------------------
   TE = I0E1 + I1E1        ; # Total exposed
   TU = I0E0 + I1E0        ; # Total unexposed
   TCA = I1E1 + I1E0       ; # Total cases
   TNC = I0E1 + I0E0       ; # Total non-cases
-
+  
   # Compute Risk
   # ---------------------------------------------------------------------------
   VAL_RE = I1E1/TE;
   VAL_RU = I1E0/TU;
   VAL_RT = TCA/(TE+TU);
-
+  
   COL_C <- c(I1E1, I1E0, TCA)
   COL_N <- c(I0E1, I0E0, TNC)
   COL_T <- c(TE, TU, TE+TU)
@@ -157,12 +181,12 @@ CS.data.frame <- function(  x,
   }
   str_PCHI2 <- sprintf("%3.3f", as.numeric(round(CHI2[2], 8)))
   str_FISH <- sprintf("%3.3f", round(FISHER, 5))
-                      
+  
   Colnames2 <- c( "Point estimate", "95%CI ll", "95%CI ul")
   COL_PES <- c(S2(VAL_RDIFF), S2(VAL_RR), S2(VAL_AFE), S2(VAL_AFP), S2(as.numeric(CHI2[1])), str_PCHI2)
   COL_CIL <- c(RD_CILOW, VAL_RR_CILOW, VAL_AFE_CILOW, NA, NA, NA)
   COL_CIH <- c(RD_CIHIG, VAL_RR_CIHIG, VAL_AFE_CIHIG, NA, NA, NA)
-
+  
   if (exact == TRUE) {
     COL_PES <- c(COL_PES, str_FISH)
     COL_CIL <- c(COL_CIL, NA)
